815bdb96a7c0c786dbf6bf2b5e4290e7313a5208
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-04-17 23:29:26 +0200

    five_step_finish

--- src/components/FeedBack/FeedBack.jsx
+++ src/components/FeedBack/FeedBack.jsx
@@ -6,6 +6,8 @@ const FeedBack = ({ good, neutral, bad, totalFeedback }) => {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>Total: {totalFeedback}</p>
+          <p>Positive: {Math.round((good / totalFeedback) * 100)}%</p>
         </div>
       ) : null}
     </>
